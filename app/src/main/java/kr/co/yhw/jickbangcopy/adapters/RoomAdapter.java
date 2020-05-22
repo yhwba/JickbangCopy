9105149b227ac9264f286d0a046aff0dc3a1c519
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-22 15:09:29 +0900

    getView메쏘도 오버라이딩

--- app/src/main/java/kr/co/yhw/jickbangcopy/adapters/RoomAdapter.java
+++ app/src/main/java/kr/co/yhw/jickbangcopy/adapters/RoomAdapter.java
@@ -2,12 +2,16 @@ package kr.co.yhw.jickbangcopy.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.List;
 
+import kr.co.yhw.jickbangcopy.R;
 import kr.co.yhw.jickbangcopy.datas.Room;
 
 public class RoomAdapter extends ArrayAdapter<Room> {
@@ -22,5 +26,17 @@ public class RoomAdapter extends ArrayAdapter<Room> {
         mList = objects;
         inf = LayoutInflater.from(mContext);
 
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView;
+
+        if (row == null){
+            row = inf.inflate(R.layout.room_list_item, null);
+
+        }
+        return row;
     }
 }
