19a7dc24ef5149d77e1a40502fe271237e2ccbe3
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-22 15:20:09 +0900

    데이터 목록 추가 + 어뎁터 생성 및 리스트뷰 연결

--- app/src/main/java/kr/co/yhw/jickbangcopy/MainActivity.java
+++ app/src/main/java/kr/co/yhw/jickbangcopy/MainActivity.java
@@ -5,16 +5,25 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
-import kr.co.yhw.jickbangcopy.databinding.ActivityMainBinding;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends BaseActivity{
+import kr.co.yhw.jickbangcopy.adapters.RoomAdapter;
+import kr.co.yhw.jickbangcopy.databinding.ActivityMainBinding;
+import kr.co.yhw.jickbangcopy.datas.Room;
+
+public class MainActivity extends BaseActivity {
 
     ActivityMainBinding binding;
+
+    List<Room> rooms = new ArrayList<>();
+    RoomAdapter mRoomAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         setupEvents();
         setValues();
     }
@@ -26,6 +35,21 @@ public class MainActivity extends BaseActivity{
 
     @Override
     public void setValues() {
+        addRooms();
+
+        mRoomAdapter = new RoomAdapter(mContext, R.layout.room_list_item, rooms);
+        binding.roomListView.setAdapter(mRoomAdapter);
+
+
+    }
+
+    void addRooms() {
+        rooms.add(new Room(7500,"은평구 불광동",4,"불광동의 흔한 집입니다."));
+        rooms.add(new Room(28500,"마포구 서교동",2,"신혼부부의 보금자리 서교동.."));
+        rooms.add(new Room(5500,"은평구 갈현동",0,"영화 기생충을 체험해보십시오"));
+        rooms.add(new Room(3500,"은평구 응암동",-1,"응암동 지하집입니다"));
+        rooms.add(new Room(58500,"은평구 신사동",5,"신사동 우리집입니다."));
+
 
     }
 }
